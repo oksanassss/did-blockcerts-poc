3d1a98f2a7ff4ecc38fd7a264c7656bd6e8edc48
Julien Fraichot <julien.fraichot@hyland.com>
2021-06-21 17:57:06 +0200

    configure bip32 for testnet

--- key-management/generateKeyPair.ts
+++ key-management/generateKeyPair.ts
@@ -6,7 +6,15 @@ const secp256k1 = require('secp256k1');
 import generateIpidDID from './generateIpidDID';
 import generateIonDID from './generateIonDID';
 
-function getPath (network: string): string {
+enum Network {
+  bitcoin = 'bitcoin',
+  testnet = 'testnet',
+  regtest = 'regtest',
+  ropsten = 'ropsten',
+  ethereum = 'ethereum'
+}
+
+function getPath (network: Network): string {
   // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
   const chainCode = {
     'bitcoin': 0,
@@ -31,19 +39,34 @@ interface IKeyPair {
   publicKey: Buffer;
 }
 
-function generateFromBip32 (network): IKeyPair {
+function generateFromBip32 (network: Network): IKeyPair {
   const mnemonic = bip39.generateMnemonic();
+  // TODO: allow for external param to be passed
+  // const mnemonic = 'prevent level element robot buyer area quick harbor input estate wrong unfair';
   console.log('mnenomic phrase generated:');
   console.log(mnemonic);
+  let bitcoinNetwork = null; // default network of bip32 is Bitcoin mainnet
+
+  if (network === 'testnet') {
+    // https://github.com/bitcoinjs/bip32/pull/7/files
+    bitcoinNetwork = {
+      wif: 0xef,
+      bip32: {
+        public: 0x043587cf,
+        private: 0x04358394
+      }
+    }
+  }
 
   const seed = bip39.mnemonicToSeedSync(mnemonic);
   console.log(seed.toString('hex'));
-  const node = bip32.fromSeed(seed);
+  const node = bip32.fromSeed(seed, bitcoinNetwork);
   const path = getPath(network);
   const derived = node.derivePath(path);
   const { privateKey, publicKey } = derived;
   console.log('private key bip32 generated', privateKey.toString('hex'), secp256k1.privateKeyVerify(privateKey));
   console.log('public key bip32 generated', publicKey.toString('hex'));
+  console.log('WIF', derived.toWIF());
 
   return {
     privateKey,
@@ -51,7 +74,7 @@ function generateFromBip32 (network): IKeyPair {
   };
 }
 
-function generateKeyPair (network = 'testnet'): IKeyPair {
+function generateKeyPair (network: Network = Network.testnet): IKeyPair {
   const { privateKey, publicKey } = generateFromBip32(network);
   return {
     privateKey,
@@ -80,11 +103,11 @@ function getEthereumAddress (publicKey: Buffer): string {
   return null;
 }
 
-function getNetwork (): string {
+function getNetwork (): Network {
   const args = process.argv;
   const networkProperty = '--network';
   const passedArgument = args.find(arg => arg.includes(networkProperty)).split('=')[1];
-  return passedArgument || 'testnet';
+  return passedArgument as Network || Network.testnet;
 }
 
 const network = getNetwork();
@@ -96,5 +119,5 @@ const ethereumAddress = getEthereumAddress(privateKey);
 console.log('ethereum address generated', ethereumAddress);
 
 // DID
-generateIpidDID(privateKey, publicKey);
-// generateIonDID(privateKey, publicKey);
+// generateIpidDID(privateKey, publicKey);
+generateIonDID(privateKey, publicKey);
